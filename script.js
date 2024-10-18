35f8e8c838a531084d1d0688a058c2daae2612b4
Sergio Joya <sejoya104@gmail.com>
2024-10-18 11:00:02 -0400

    Create GameBoard function factory

---
+++ script.js
@@ -0,0 +1,31 @@
+function GameBoard() {
+    const row = 3;
+    const column = 3;
+    const board = [];
+
+    for (let i = 0; i < row; i++) {
+        board[i] = [];
+        for (let j = 0; j < column; j++) {
+            board[i][j] = null;
+        }
+    }
+    
+    function printBoard() {
+        console.log(board);
+    }
+
+    function placeMarker(row, column, player) {
+        if (
+            (row < 0 || row >= board.length) ||
+            (column < 0 || column >= board.length)
+        ) {
+            console.log('ERROR! Index out of range');
+        } else if (!board[row][column]) {
+            board[row][column] = player;
+        } else {
+            console.log('ERROR! The square is already filled in');
+        }
+    }
+
+    return {printBoard, placeMarker};
+}
